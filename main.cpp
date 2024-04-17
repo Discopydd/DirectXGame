b8cd24b325afebb99e11b9724962098469187816
勇 李 <2870231623@qq.com>
2024-04-17 17:10:27 +0900

    Complete create D3D12Device!!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,156 @@
 #include<Windows.h>
+#include<cstdint>
+#include<string>
+#include<format>
 
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#include<cassert>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	
+	switch (msg) {
+
+	case WM_DESTROY:
+
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+void Log(const std::string & message){
+		OutputDebugStringA(message.c_str());
+	}
+std::wstring ConvertString(const std::string& str) {
+    if (str.empty()) {
+        return std::wstring();
+    }
+
+    auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+    if (sizeNeeded == 0) {
+        return std::wstring();
+    }
+    std::wstring result(sizeNeeded, 0);
+    MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+    return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+    if (str.empty()) {
+        return std::string();
+    }
+
+    auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+    if (sizeNeeded == 0) {
+        return std::string();
+    }
+    std::string result(sizeNeeded, 0);
+    WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+    return result;
+}
 int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
+	
+#pragma region Windowの生成
+
+	WNDCLASS wc{};
+
+	wc.lpfnWndProc = WindowProc;
+	wc.lpszClassName = L"CG2WindowClass";
+	wc.hInstance = GetModuleHandle(nullptr);
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	RegisterClass(&wc);
+
+	const int32_t kCljientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	RECT wrc = { 0,0,kCljientWidth,kClientHeight };
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,
+		L"CG2",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr
+	);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+#pragma endregion
+
+	IDXGIFactory7* dxgiFactory = nullptr;
+
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	assert(SUCCEEDED(hr));
+
+		//使用するアダプタ用の変数。初にnullptrを入れておく
+	IDXGIAdapter4* useAdapter = nullptr;
+
+	//良い順にアダプタを頼む
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) !=
+		DXGI_ERROR_NOT_FOUND; ++i) {
+		//アダプターの情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr)); // 取得できないのは一大事
+		//ソフトウエアアダプタでなければ採用！
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {//採用したアダプタの情報をログに出力。wstringの方なので注意
+			Log(ConvertString(std::format(L"Use Adapater:{}\n", adapterDesc.Description)));
+			break;
+		}
+		useAdapter = nullptr;//ソフトウエアアダプタの場合は見なかったことにする
+	}
+    //適切なアダプタが見つからなかったので起動できない
+	assert(useAdapter != nullptr);
+
+
+	ID3D12Device* device = nullptr;
+	//機能レ7レとログ出力用の文字列
+	D3D_FEATURE_LEVEL featureLevels[] = {
+	D3D_FEATURE_LEVEL_12_2, D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+	};
+	const char* featureLeveLStrings[] = { "12.2","12.1","12.0" };
+    //高い順に生成できるか試していく
+	for (size_t i = 0; i < _countof(featureLevels); ++i) {
+		//採用したアタプターでデバイスを生成
+		hr = D3D12CreateDevice(useAdapter,
+			featureLevels[i], IID_PPV_ARGS(&device));
+		//指定した機能レ7レでデバイスが生成できたかを確認
+		if (SUCCEEDED(hr)) {
+			//生成できたのでログ出力を行ってループを扱ける
+			Log(std::format("FeatureLevel : {}\n",
+				featureLeveLStrings[i]));
+			break;
+		}
+	}
+//デバイスの生成がうまくいかなかったので起動できない
+assert(device != nullptr);
+Log("CompLete create D3D12Device!!!\n");//初期化完了のログをだす
+
+	MSG msg{};
+	while (msg.message != WM_QUIT) {
+
+		if (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessageW(&msg);
+		}
+		else {
+			//ゲーム処理
+		}
+	}
 	OutputDebugStringA("Hello,DirectX\n");
 
 	return 0;
