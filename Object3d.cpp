52f6489a39eb3565690c96cb20b5df0c46cbe631
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2025-01-20 14:16:15 +0900

    06_04

--- Object3d.cpp
+++ Object3d.cpp
@@ -1,1 +1,58 @@
 #include"Object3d.h"
+#include "math/MyMath.h"
+
+void Object3d::Initialize(Object3dCommon* object3dCommon)
+{
+	this->object3dCommon_ = object3dCommon;
+
+	#pragma region ModelTransform
+	//ModelTransform用のリソースを作る。Matrix4x4 1つ分のサイズを用意する
+	transformationMatrixResource = object3dCommon_->GetDxCommon()->CreateBufferResource(sizeof(TransformationMatrix));
+	//書き込むためのアドレスを取得
+	transformationMatrixResource->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixData));
+	//単位行列を書き込む
+	transformationMatrixData->WVP = Math::MakeIdentity4x4();
+	transformationMatrixData->World = Math::MakeIdentity4x4();
+#pragma endregion
+	
+
+#pragma region 平行光源
+	//平行光源用のResourceを作成
+	directionalLightResource = object3dCommon_->GetDxCommon()->CreateBufferResource(sizeof(DirectionalLight));
+	directionalLightResource->Map(0, nullptr, reinterpret_cast<void**>(&directionalLightData));
+	directionalLightData->color = { 1.0f,1.0f,1.0f,1.0f };
+	directionalLightData->direction = { 0.0f,-1.0f,1.0f };
+	directionalLightData->intensity = 1.0f;
+#pragma endregion
+	
+	//カメラとモデルのTransform変数
+	transform = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f} ,{1.0f,0.0f,0.0f} };
+
+	//カメラ用のTransformを作る
+	cameraTransform = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f} ,{ 0.0f,0.0f,-10.0f} };
+}
+
+void Object3d::Update()
+{
+
+	Matrix4x4 worldMatrix = Math::MakeAffineMatrix(transform.scale, transform.rotate, transform.translate);
+	Matrix4x4 cameraMatrix = Math::MakeAffineMatrix(cameraTransform.scale, cameraTransform.rotate, cameraTransform.translate);
+	Matrix4x4 viewMatrix = Math::Inverse(cameraMatrix);
+	Matrix4x4 projectionMatrix = Math::MakePerspectiveFovMatrix(0.45f, float(WinApp::kClientWidth) / float(WinApp::kClientHeight), 0.1f, 100.0f);
+	Matrix4x4 worldViewProjectionMatrix = Math::Multiply(worldMatrix, Math::Multiply(viewMatrix, projectionMatrix));
+	transformationMatrixData->WVP = worldViewProjectionMatrix;
+	transformationMatrixData->World = worldMatrix;
+}
+
+void Object3d::Draw()
+{
+	object3dCommon_->GetDxCommon()->GetCommandList()->SetGraphicsRootConstantBufferView(1, transformationMatrixResource->GetGPUVirtualAddress());
+	//平行光源Cbufferの場所を設定
+	object3dCommon_->GetDxCommon()->GetCommandList()->SetGraphicsRootConstantBufferView(3, directionalLightResource->GetGPUVirtualAddress());
+
+	if (model_) {
+		model_->Draw();
+	}
+}
+
+
--- Object3d.h
+++ Object3d.h
@@ -1,9 +1,56 @@
 #pragma once
+#include "Object3dCommon.h"
+#include "math/Matrix4x4.h"
+#include "math/Vector4.h"
+#include "math/Vector3.h"
+#include "math/Vector2.h"
+#include "ReadingData.h"
+#include "Model.h"
 
+class Object3dCommon;
 class Object3d {
 
 public:
 
-	void Initialize();
+	void Initialize(Object3dCommon* object3dCommon);
+	// 更新
+	void Update();
 
+	// 描画
+	void Draw();
+
+
+	void SetModel(Model* model) { model_ = model; }
+	void SetModel(const std::string& filepath);
+
+	//transform
+	void SetTransform(const Transform& transform) { this->transform = transform; }
+	Transform GetTransform() { return transform; }
+
+	//スケール
+	void SetScale(const Vector3& scale) { transform.scale = scale; }
+	//回転
+	void SetRotate(const Vector3& rotate) { transform.rotate = rotate; }
+	//位置
+	void SetTranslate(const Vector3& translate) { transform.translate = translate; }
+
+private:
+
+	Object3dCommon* object3dCommon_ = nullptr;
+	// モデル
+	Model* model_ = nullptr;
+
+	// ModelTransform用のリソースを作る。Matrix4x4 1つ分のサイズを用意する
+	Microsoft::WRL::ComPtr<ID3D12Resource> transformationMatrixResource;
+	// データを書き込む
+	TransformationMatrix* transformationMatrixData = nullptr;
+
+	// 平行光源用のResourceを作成
+	Microsoft::WRL::ComPtr<ID3D12Resource> directionalLightResource;
+	DirectionalLight* directionalLightData = nullptr;
+
+	// SRT
+	Transform transform;
+	// カメラ用のTransformを作る
+	Transform cameraTransform;
 };
